d18332bb254fc3a18b6fc14485ca05dfc895b152
matiqof <matiqof@gmail.com>
2024-10-10 23:16:51 +0300

    Создание проекта

---
+++ src/main/java/com/example/stankingibdd/StankinGibddApplication.java
@@ -0,0 +1,13 @@
+package com.example.stankingibdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StankinGibddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StankinGibddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stankingibdd/StankinGibddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stankingibdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StankinGibddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
